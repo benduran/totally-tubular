648c535e87010426ef3bf0596c55e24a8fd98b5b
benduran <stratodyne@gmail.com>
2025-05-26 15:19:15 +0100

    chore: did some nasty suppression of typescript, but it works

--- src/totally-tubular.ts
+++ src/totally-tubular.ts
@@ -1,4 +1,4 @@
-import { AllObjectKeys, PropType } from './types.js';
+import { AllObjectKeys, ObservationCallback, PropType } from './types.js';
 
 /**
  * A totally tubular instance that tracks a top-level
@@ -7,10 +7,36 @@ import { AllObjectKeys, PropType } from './types.js';
  * you should construct your state to be a series of Tubular instances
  */
 export class Tubular<T extends object> {
+  private observationCallbacks: Map<AllObjectKeys<T>, ObservationCallback<T>[]> = new Map();
+
   private state: T;
 
   constructor(initialState: T) {
     this.state = initialState;
+  }
+
+  private doSomethingToPath<K extends AllObjectKeys<T>, V = PropType<T, K & string>>(
+    readPath: K,
+    doSomething: (currVal: V) => V,
+  ) {
+    let thing: any = this.state;
+
+    if (typeof readPath !== 'string') {
+      return this.getPortionOfStateByPath(thing[readPath]);
+    }
+
+    const splitPath = readPath.split('.');
+
+    const len = splitPath.length;
+    for (let i = 0; i < len; i++) {
+      const p = splitPath[i] || '';
+      if (i === len - 1) {
+        if (thing) thing[p] = doSomething(thing[p]);
+      } else thing = thing?.[p];
+    }
+    for (const p of splitPath) {
+      thing = thing?.[p];
+    }
   }
 
   private getPortionOfStateByPath<K extends AllObjectKeys<T>, V = PropType<T, K & string>>(readPath: K): V | null {
@@ -30,6 +56,18 @@ export class Tubular<T extends object> {
   }
 
   /**
+   * observe changes to some part of state that was updated
+   */
+  observe<K extends AllObjectKeys<T>, V = PropType<T, K & string>>(
+    propPath: K,
+    observeCallback: (newVal: V, oldVal: V) => void,
+  ) {
+    const prevForPath = this.observationCallbacks.get(propPath) ?? [];
+    // @ts-expect-error - shut up, please
+    this.observationCallbacks.set(propPath, [...prevForPath, observeCallback]);
+  }
+
+  /**
    * reads a portion of your state
    */
   read<K extends AllObjectKeys<T>>(readPath: K): PropType<T, K & string> | null {
@@ -39,7 +77,22 @@ export class Tubular<T extends object> {
   /**
    * updates a portion of your state
    */
-  update<K extends AllObjectKeys<T>>(updatePath: K, val: PropType<T, K & string>) {}
+  update<K extends AllObjectKeys<T>>(
+    updatePath: K,
+    valUpdater: (oldVal: PropType<T, K & string>) => PropType<T, K & string>,
+  ) {
+    const prevVal = this.read(updatePath);
+    let newVal: typeof prevVal | null = null;
+
+    this.doSomethingToPath(updatePath, oldVal => {
+      const n = valUpdater(oldVal);
+      newVal = n;
+      return n;
+    });
+
+    // @ts-expect-error - whatever
+    this.observationCallbacks.get(updatePath)?.forEach(cb => cb(newVal, prevVal));
+  }
 }
 interface MyThing {
   drink: {
@@ -65,4 +118,6 @@ const initialState: MyThing = {
 
 const t = new Tubular(initialState);
 
-console.info(t.read('drink2.kind'));
+t.observe('drink.beer', (newVal, oldVal) => console.info('newVal', newVal, 'oldVal', oldVal));
+t.observe('drink', newVal => console.info('new val is', newVal));
+t.update('drink', old => ({ ...old, kind: 'pisswater' }));
